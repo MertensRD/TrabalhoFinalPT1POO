f922a37538e6c22d7d4c84572ae27a6968655a26
Korrigao <kiluynam.roo@gmail.com>
2026-07-13 14:54:53 -0400

    Primeiro Commit

---
+++ TrabalhoFinalPT1POO/src/main/java/turing/trabalhofinalpt1poo/TrabalhoFinalPT1POO.java
@@ -0,0 +1,10 @@
+package turing.trabalhofinalpt1poo;
+
+
+public class TrabalhoFinalPT1POO 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+}
